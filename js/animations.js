b941a2ccc15ab487870e61a8d6e063ffb70873d4
ventas7-rgb <ventas7@catekomsas.com>
2026-07-10 13:26:57 -0500

    Delete js directory

--- js/animations.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  if (window.AOS) {
-    window.AOS.init({
-      duration: 700,
-      once: true,
-      easing: 'ease-out-cubic'
-    });
-  }
-});
--- js/whatsapp.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const whatsappLink = document.querySelector('.whatsapp-float');
-  whatsappLink?.addEventListener('click', () => {
-    window.open('https://wa.me/573001112233', '_blank', 'noopener,noreferrer');
-  });
-});
